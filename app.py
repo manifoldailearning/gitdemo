6a97f546fb1982365d3e69e583b9431ffabcf77c
nachiketh <murthy@manifoldailearning.in>
2026-03-14 22:03:50 +0530

    github actions

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
-print("Hello, World!")
-print("hello from feature 1 branch")
-print("hello from feature 2 branch")
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+print("installation successful")
